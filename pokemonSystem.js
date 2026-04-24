dd33754f20974f6625c8c2ae9b27542ecc06ad27
Tung Tran <teecutea@gmail.com>
2026-04-23 20:06:06 -0400

    installed 3 npm modules (Express bodyparser and ejs)

--- pokemonSystem.js
+++ pokemonSystem.js
@@ -1,2 +1,12 @@
 "use strict";
 
+const express = require("express");
+const app = express();
+const path = require("path");
+const portNumber = 7003;
+const bodyParser = require("body-parser");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.set("view engine", "ejs");
+app.set("views", path.resolve(__dirname, "templates"));
